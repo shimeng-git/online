6ab78e6c68cb54f1d92160b939eb223f3df10669
shi meng <shimengfox@foxmail.com>
2021-04-09 13:15:44 +0800

    123

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<vector>
+#include<string>
+#include<stack>
+#include<queue>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
