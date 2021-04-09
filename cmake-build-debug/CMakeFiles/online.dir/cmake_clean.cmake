0249c9e426cdf991bb053a37cc42ec7121dcab85
shi meng <shimengfox@foxmail.com>
2021-04-09 14:20:41 +0800

    'add'

---
+++ cmake-build-debug/CMakeFiles/online.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/online.dir/main.cpp.obj"
+  "online.exe"
+  "online.exe.manifest"
+  "online.lib"
+  "online.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/online.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
