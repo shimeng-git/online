c02dc5e0967ef04c74c5fb438694983491ba4688
shi meng <shimengfox@foxmail.com>
2021-04-09 15:32:23 +0800

    'a'

--- main.cpp
+++ main.cpp
@@ -9,8 +9,36 @@ using namespace std;
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+    vector<long long> data(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> data[i];
+    }
+    if (n < 3) {
+        cout << 0 << endl;
+        return 0;
+    }
 
+    vector<int> bottom;
+    if (data[0] < data[1]) {
+        bottom.push_back(0);
+    }
+    for (int i = 1; i < n - 1; ++i) {
+        if (data[i] < data[i - 1] && data[i] < data[i + 1]) {
+            bottom.push_back(i);
+        }
+    }
+    if (data[n-1] < data[n-2]) {
+        bottom.push_back(n-1);
+    }
+
+    int res = 0;
+    for (int i = 1; i < bottom.size(); ++i) {
+        res = max(bottom[i] - bottom[i - 1] + 1, res);
+
+    }
+    cout << res << endl;
 
     return 0;
 }
