7934094e7b387745f0a51f6f22cb665147901c7f
shi meng <shimengfox@foxmail.com>
2021-04-17 20:18:40 +0800

    first

--- miyayo.cpp
+++ miyayo.cpp
@@ -13,9 +13,33 @@
 using namespace std;
 
 
+
+
 int main()
 {
+    int m, n;
+    cin >> n >> m;
+    vector<int> record(n);
+    for (int i = 0; i < record.size(); ++i) {
+        cin >> record[i];
+    }
+    sort(record.begin(), record.end(), greater<>());
 
+    int res = 0;
+    for (int i = 0; i < record.size(); ++i) {
+        int temp = res + record[i];
+        if (temp < m) {
+            res = temp;
+        } else {
+            int cur = record[i];
+            while (i < record.size() && record[i] + res >= m) {
+                cur = record[i];
+                ++i;
+            }
+            res += cur;
+        }
+    }
+    cout << res << endl;
 
     return 0;
 }
